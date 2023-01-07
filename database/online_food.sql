133fe16fcc1e54c99725770eeb91208aec5f971b
Om Kalariya <omkalariya2002@gmail.com>
2023-01-07 11:11:15 +0530

    -- 07/jan/2023

---
+++ database/online_food.sql
@@ -0,0 +1,620 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 09, 2021 at 05:53 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `online_food`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `name`, `username`, `password`, `email`) VALUES
+(1, 'Admin', 'admin', 'admin', 'admin@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `banner`
+--
+
+CREATE TABLE `banner` (
+  `id` int(11) NOT NULL,
+  `image` varchar(100) NOT NULL,
+  `heading` varchar(500) NOT NULL,
+  `sub_heading` varchar(500) NOT NULL,
+  `link` varchar(100) NOT NULL,
+  `link_txt` varchar(100) NOT NULL,
+  `order_number` int(11) NOT NULL,
+  `added_on` datetime NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `banner`
+--
+
+INSERT INTO `banner` (`id`, `image`, `heading`, `sub_heading`, `link`, `link_txt`, `order_number`, `added_on`, `status`) VALUES
+(1, '533799913_banner-4.jpg', 'Drink & Heathy Food', 'Fresh Heathy and Organic', 'shop', 'Order Now', 1, '2021-10-28 03:00:05', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL,
+  `category` varchar(50) NOT NULL,
+  `order_number` int(11) NOT NULL,
+  `status` int(11) NOT NULL,
+  `added_on` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`id`, `category`, `order_number`, `status`, `added_on`) VALUES
+(1, 'Chaat & Snacks', 1, 1, '2021-10-28 12:06:33'),
+(2, 'Chinese', 2, 1, '2021-10-21 12:06:41'),
+(3, 'South Indian', 3, 1, '2021-10-28 12:06:59'),
+(4, 'Desserts', 4, 1, '2021-10-28 12:07:18'),
+(5, 'Murg', 0, 1, '2021-10-28 12:49:37'),
+(6, 'pizza', 6, 1, '2021-10-28 02:48:41');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact_us`
+--
+
+CREATE TABLE `contact_us` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `mobile` varchar(15) NOT NULL,
+  `subject` varchar(200) NOT NULL,
+  `message` text NOT NULL,
+  `added_on` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `coupon_code`
+--
+
+CREATE TABLE `coupon_code` (
+  `id` int(11) NOT NULL,
+  `coupon_code` varchar(20) NOT NULL,
+  `coupon_type` enum('P','F') NOT NULL,
+  `coupon_value` int(11) NOT NULL,
+  `cart_min_value` int(11) NOT NULL,
+  `expired_on` date NOT NULL,
+  `status` int(11) NOT NULL,
+  `added_on` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `coupon_code`
+--
+
+INSERT INTO `coupon_code` (`id`, `coupon_code`, `coupon_type`, `coupon_value`, `cart_min_value`, `expired_on`, `status`, `added_on`) VALUES
+(1, 'FIRST50', 'P', 10, 50, '2021-12-31', 1, '2021-10-28 05:31:03'),
+(2, 'FRIDAY', 'F', 200, 200, '2021-12-31', 1, '2021-10-21 10:38:43');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `delivery_boy`
+--
+
+CREATE TABLE `delivery_boy` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `mobile` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `status` int(11) NOT NULL,
+  `added_on` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `delivery_boy`
+--
+
+INSERT INTO `delivery_boy` (`id`, `name`, `mobile`, `password`, `status`, `added_on`) VALUES
+(1, 'Vishal', '1234567890', '123', 1, '2021-10-28 08:06:06');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dish`
+--
+
+CREATE TABLE `dish` (
+  `id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `dish` varchar(100) NOT NULL,
+  `dish_detail` text NOT NULL,
+  `image` varchar(100) NOT NULL,
+  `type` enum('veg','non-veg') NOT NULL,
+  `status` int(11) NOT NULL,
+  `added_on` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `dish`
+--
+
+INSERT INTO `dish` (`id`, `category_id`, `dish`, `dish_detail`, `image`, `type`, `status`, `added_on`) VALUES
+(1, 4, 'Gulab Jamun', 'Gulab Jamun', '977945963_862169053_gulab-jamun.jpg', 'veg', 1, '2021-10-28 10:43:59'),
+(3, 2, 'Chow mein', 'Chow mein', '836724175_Chowmein.jpg', 'non-veg', 1, '2021-10-28 10:47:26'),
+(4, 5, 'Butter Chicken', 'Butter chicken or murg makhani is a dish, originating in the Indian subcontinent, of chicken in a mildly spiced tomato sauce.', '348714192_30-Minute-Instant-Pot-Butter-Chicken-7.jpg', 'non-veg', 1, '2021-10-28 12:50:50'),
+(5, 2, 'Testing', 'testing', '140977647_404.jpg', 'veg', 1, '2021-10-28 12:00:24'),
+(6, 6, 'vegetable pizza', 'mix vegatable', '924465404_pizza.jpg', 'veg', 1, '2021-10-28 02:49:49');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dish_cart`
+--
+
+CREATE TABLE `dish_cart` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `dish_detail_id` int(11) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `added_on` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `dish_cart`
+--
+
+INSERT INTO `dish_cart` (`id`, `user_id`, `dish_detail_id`, `qty`, `added_on`) VALUES
+(3, 2, 6, 2, '2021-10-28 09:18:31');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dish_details`
+--
+
+CREATE TABLE `dish_details` (
+  `id` int(11) NOT NULL,
+  `dish_id` int(11) NOT NULL,
+  `attribute` varchar(100) NOT NULL,
+  `price` int(11) NOT NULL,
+  `status` int(11) NOT NULL,
+  `added_on` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `dish_details`
+--
+
+INSERT INTO `dish_details` (`id`, `dish_id`, `attribute`, `price`, `status`, `added_on`) VALUES
+(1, 3, 'Full', 300, 1, '2021-10-28 10:25:47'),
+(2, 3, 'Half', 170, 1, '2021-10-28 10:49:45'),
+(6, 1, 'Per Piece', 40, 1, '2021-10-28 00:00:00'),
+(8, 4, 'Half', 250, 0, '2021-10-28 12:50:50'),
+(9, 4, 'Full', 410, 1, '2021-10-28 12:50:50'),
+(11, 5, 'Test1', 100, 1, '2021-10-28 12:00:24'),
+(13, 6, 'medium', 150, 1, '2021-10-28 02:49:49');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_detail`
+--
+
+CREATE TABLE `order_detail` (
+  `id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `dish_details_id` int(11) NOT NULL,
+  `price` float NOT NULL,
+  `qty` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `order_detail`
+--
+
+INSERT INTO `order_detail` (`id`, `order_id`, `dish_details_id`, `price`, `qty`) VALUES
+(1, 1, 6, 40, 6),
+(2, 2, 6, 40, 4),
+(3, 3, 6, 40, 3),
+(4, 4, 6, 40, 1),
+(5, 5, 6, 40, 1),
+(6, 5, 2, 170, 1),
+(7, 6, 13, 150, 1),
+(8, 7, 6, 40, 1),
+(9, 8, 6, 40, 1),
+(10, 9, 13, 150, 1),
+(11, 9, 9, 410, 2),
+(12, 10, 13, 150, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_master`
+--
+
+CREATE TABLE `order_master` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `mobile` varchar(50) NOT NULL,
+  `address` text NOT NULL,
+  `total_price` float NOT NULL,
+  `coupon_code` varchar(20) NOT NULL,
+  `final_price` float NOT NULL,
+  `zipcode` varchar(10) NOT NULL,
+  `delivery_boy_id` int(11) NOT NULL,
+  `payment_status` varchar(20) NOT NULL,
+  `payment_type` varchar(10) NOT NULL,
+  `payment_id` varchar(100) NOT NULL,
+  `order_status` int(11) NOT NULL,
+  `cancel_by` enum('user','admin') NOT NULL,
+  `cancel_at` datetime NOT NULL,
+  `added_on` datetime NOT NULL,
+  `delivered_on` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `order_master`
+--
+
+INSERT INTO `order_master` (`id`, `user_id`, `name`, `email`, `mobile`, `address`, `total_price`, `coupon_code`, `final_price`, `zipcode`, `delivery_boy_id`, `payment_status`, `payment_type`, `payment_id`, `order_status`, `cancel_by`, `cancel_at`, `added_on`, `delivered_on`) VALUES
+(6, 6, 'om1', 'om@gmail.com', '1234567890', 'tygj', 150, '', 150, '360004', 1, 'success', 'cod', '', 4, 'user', '0000-00-00 00:00:00', '2021-10-28 03:05:59', '2021-10-29 09:17:31'),
+(7, 6, 'om1', 'om@gmail.com', '1234567890', 'sdds', 40, '', 40, '360004', 1, 'success', 'cod', '', 4, 'user', '2021-10-29 09:18:14', '2021-10-29 09:14:16', '2021-10-29 09:23:54'),
+(8, 6, 'om1', 'om@gmail.com', '1234567890', 'sdds', 40, '', 40, '360004', 0, 'pending', 'cod', '', 1, 'user', '0000-00-00 00:00:00', '2021-10-29 09:21:31', '0000-00-00 00:00:00'),
+(9, 6, 'om1', 'om@gmail.com', '1234567890', 'thf', 970, '', 970, '360004', 1, 'success', 'cod', '', 4, 'user', '0000-00-00 00:00:00', '2021-10-30 11:14:11', '2021-10-30 11:17:50'),
+(10, 6, 'om1', 'om@gmail.com', '1234567890', 'tf', 300, '', 300, '360004', 1, 'success', 'cod', '', 4, 'user', '0000-00-00 00:00:00', '2021-11-09 10:09:00', '2021-11-09 10:12:38');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_status`
+--
+
+CREATE TABLE `order_status` (
+  `id` int(11) NOT NULL,
+  `order_status` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `order_status`
+--
+
+INSERT INTO `order_status` (`id`, `order_status`) VALUES
+(1, 'Pending'),
+(2, 'Cooking '),
+(3, 'On the Way'),
+(4, 'Delivered'),
+(5, 'Cancel');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rating`
+--
+
+CREATE TABLE `rating` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `dish_detail_id` int(11) NOT NULL,
+  `rating` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `rating`
+--
+
+INSERT INTO `rating` (`id`, `user_id`, `order_id`, `dish_detail_id`, `rating`) VALUES
+(1, 1, 6, 1, 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `setting`
+--
+
+CREATE TABLE `setting` (
+  `id` int(11) NOT NULL,
+  `cart_min_price` int(11) NOT NULL,
+  `cart_min_price_msg` varchar(250) NOT NULL,
+  `website_close` int(11) NOT NULL,
+  `wallet_amt` int(11) NOT NULL,
+  `website_close_msg` varchar(250) NOT NULL,
+  `referral_amt` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `setting`
+--
+
+INSERT INTO `setting` (`id`, `cart_min_price`, `cart_min_price_msg`, `website_close`, `wallet_amt`, `website_close_msg`, `referral_amt`) VALUES
+(1, 40, 'Cart min price will be 40 rs', 0, 0, 'Website Closed for today', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `mobile` varchar(15) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `status` int(11) NOT NULL,
+  `email_verify` int(11) NOT NULL,
+  `rand_str` varchar(20) NOT NULL,
+  `referral_code` varchar(20) NOT NULL,
+  `from_referral_code` varchar(20) NOT NULL,
+  `added_on` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `email`, `mobile`, `password`, `status`, `email_verify`, `rand_str`, `referral_code`, `from_referral_code`, `added_on`) VALUES
+(6, 'om1', 'om@gmail.com', '1234567890', '$2y$10$AT4nF/j/4CSYD2Na0zPS0uPu2oLjqcxb7h6kuyWG8rBIRj94E0mD.', 1, 1, 'meuodbgdspiqrcs', 'koywtxugzoljmie', '', '2021-10-28 10:58:17'),
+(12, '1', 'om476@gmail.com', '3331', '$2y$10$dthtcv/3WHJh8lPao03G5e8ssCKx2eD2R8SCda7KkQviIZxrNjM3K', 1, 0, 'vqstowlidxrcjkm', 'fzoidglgsedmkhu', '', '2021-10-29 09:25:19');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wallet`
+--
+
+CREATE TABLE `wallet` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `amt` int(11) NOT NULL,
+  `msg` varchar(500) NOT NULL,
+  `type` enum('in','out') NOT NULL,
+  `payment_id` varchar(50) NOT NULL,
+  `added_on` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `banner`
+--
+ALTER TABLE `banner`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `contact_us`
+--
+ALTER TABLE `contact_us`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `coupon_code`
+--
+ALTER TABLE `coupon_code`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `delivery_boy`
+--
+ALTER TABLE `delivery_boy`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `dish`
+--
+ALTER TABLE `dish`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `dish_cart`
+--
+ALTER TABLE `dish_cart`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `dish_details`
+--
+ALTER TABLE `dish_details`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `order_detail`
+--
+ALTER TABLE `order_detail`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `order_master`
+--
+ALTER TABLE `order_master`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `order_status`
+--
+ALTER TABLE `order_status`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `rating`
+--
+ALTER TABLE `rating`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `setting`
+--
+ALTER TABLE `setting`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `wallet`
+--
+ALTER TABLE `wallet`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `banner`
+--
+ALTER TABLE `banner`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `contact_us`
+--
+ALTER TABLE `contact_us`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `coupon_code`
+--
+ALTER TABLE `coupon_code`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `delivery_boy`
+--
+ALTER TABLE `delivery_boy`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `dish`
+--
+ALTER TABLE `dish`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `dish_cart`
+--
+ALTER TABLE `dish_cart`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `dish_details`
+--
+ALTER TABLE `dish_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `order_detail`
+--
+ALTER TABLE `order_detail`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `order_master`
+--
+ALTER TABLE `order_master`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `order_status`
+--
+ALTER TABLE `order_status`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `rating`
+--
+ALTER TABLE `rating`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `setting`
+--
+ALTER TABLE `setting`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `wallet`
+--
+ALTER TABLE `wallet`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
